cbc139ac784529ef606d8974996733e316a305cb
Ruda Yao <rudayao@usc.edu>
2020-11-06 01:58:41 +0800

    1

--- pytorch/model.py
+++ pytorch/model.py
@@ -15,20 +15,20 @@ class Ruda_Model(nn.Module):
     def __init__(self):
         super(Ruda_Model, self).__init__()
 
-        self.conv1_1 = nn.Conv2d(3, 64, 11, padding=1)
-        self.conv1_2 = nn.Conv2d(64, 64, 11, padding=1)
+        self.conv1_1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
         self.bn1_1 = nn.BatchNorm2d(64)
         self.bn1_2 = nn.BatchNorm2d(64)
         self.pool1 = nn.MaxPool2d(kernel_size=2)
 
-        self.conv2_1 = nn.Conv2d(64, 128, 5, padding=1)
-        self.conv2_2 = nn.Conv2d(128, 128, 5, padding=1)
+        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
         self.bn2_1 = nn.BatchNorm2d(128)
         self.bn2_2 = nn.BatchNorm2d(128)
         self.pool2 = nn.MaxPool2d(kernel_size=2)
 
-        self.conv3_1 = nn.Conv2d(128, 256, 5, padding=1)
-        self.conv3_2 = nn.Conv2d(256, 256, 5, padding=1)
+        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
         self.bn3_1 = nn.BatchNorm2d(256)
         self.bn3_2 = nn.BatchNorm2d(256)
         self.pool3 = nn.MaxPool2d(kernel_size=2)
@@ -47,11 +47,12 @@ class Ruda_Model(nn.Module):
         # self.pool4 = nn.MaxPool2d(kernel_size=2, dilation=2)
         self.pool5 = nn.MaxPool2d(kernel_size=2)
 
-        self.dropout = nn.Dropout2d(0.1)
+        self.dropout_conv = nn.Dropout2d(0.1)
+        self.dropout_fc = nn.Dropout2d(0.5)
 
-        self.fc1 = nn.Linear(256 * 5 * 5, 153)
-        # self.fc2 = nn.Linear(2048, 2048)
-        # self.fc3 = nn.Linear(2048, 153)
+        self.fc1 = nn.Linear(256 * 5 * 5, 2048)
+        self.fc2 = nn.Linear(2048, 2048)
+        self.fc3 = nn.Linear(2048, 153)
 
     def forward(self, x):
         out = x
@@ -66,7 +67,7 @@ class Ruda_Model(nn.Module):
         out = self.bn1_2(out)
         out = self.pool1(out)
         out = F.relu(out)
-        out = self.dropout(out)
+        out = self.dropout_conv(out)
 
         out = self.conv2_1(out)
         out = self.bn2_1(out)
@@ -74,7 +75,7 @@ class Ruda_Model(nn.Module):
         out = self.bn2_2(out)
         out = self.pool2(out)
         out = F.relu(out)
-        out = self.dropout(out)
+        out = self.dropout_conv(out)
 
         out = self.conv3_1(out)
         out = self.bn3_1(out)
@@ -82,7 +83,7 @@ class Ruda_Model(nn.Module):
         out = self.bn3_2(out)
         out = self.pool3(out)
         out = F.relu(out)
-        out = self.dropout(out)
+        out = self.dropout_conv(out)
 
         out = self.conv4_1(out)
         out = self.bn4_1(out)
@@ -90,7 +91,7 @@ class Ruda_Model(nn.Module):
         out = self.bn4_2(out)
         out = self.pool4(out)
         out = F.relu(out)
-        out = self.dropout(out)
+        out = self.dropout_conv(out)
 
         out = self.conv5_1(out)
         out = self.bn5_1(out)
@@ -98,7 +99,7 @@ class Ruda_Model(nn.Module):
         out = self.bn5_2(out)
         out = self.pool5(out)
         out = F.relu(out)
-        out = self.dropout(out)
+        out = self.dropout_conv(out)
 
         # flatten
         # print(out.shape)
@@ -107,8 +108,10 @@ class Ruda_Model(nn.Module):
 
 
         out = self.fc1(out)
-        # out = self.fc2(out)
-        # out = self.fc3(out)
+        out = self.dropout_fc(out)
+        out = self.fc2(out)
+        out = self.dropout_fc(out)
+        out = self.fc3(out)
 
         # out = F.sigmoid(out)
         return out
