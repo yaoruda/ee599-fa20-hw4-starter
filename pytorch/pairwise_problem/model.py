4e8b2c01fb297f59b8ec22c52ab4e06862b19935
Ruda Yao <rudayao@usc.edu>
2020-11-06 17:23:01 +0800

    1

--- pytorch/pairwise_problem/model.py
+++ pytorch/pairwise_problem/model.py
@@ -17,11 +17,11 @@ class Ruda_Model(nn.Module):
     def __init__(self):
         super(Ruda_Model, self).__init__()
 
-        self.conv1_1 = nn.Conv2d(6, 24, 3, stride=4, bias=False)
-        self.conv1_2 = nn.Conv2d(24, 48, 3, stride=4, bias=False)
-        self.bn1_1 = nn.BatchNorm2d(24)
-        self.bn1_2 = nn.BatchNorm2d(48)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, dilation=4)
+        self.conv1_1 = nn.Conv2d(6, 48, 3, stride=4, bias=False)
+        self.conv1_2 = nn.Conv2d(48, 96, 3, stride=4, bias=False)
+        self.bn1_1 = nn.BatchNorm2d(48)
+        self.bn1_2 = nn.BatchNorm2d(96)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, dilation=2)
 
         self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
         self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
@@ -51,7 +51,7 @@ class Ruda_Model(nn.Module):
 
         self.dropout = nn.Dropout2d(0.1)
 
-        self.fc1 = nn.Linear(24 * 26 * 26, 512)
+        self.fc1 = nn.Linear(96 * 6 * 6, 512)
         self.fc2 = nn.Linear(512, 1)
         # self.fc3 = nn.Linear(2048, 153)
 
@@ -62,11 +62,11 @@ class Ruda_Model(nn.Module):
 
         out = self.conv1_1(out)
         out = self.bn1_1(out)
-        # out = self.conv1_2(out)
-        # out = self.bn1_2(out)
+        out = self.conv1_2(out)
+        out = self.bn1_2(out)
         out = self.pool1(out)
-        # out = F.relu(out)
-        # out = self.dropout(out)
+        out = F.relu(out)
+        out = self.dropout(out)
 
         # out = self.conv2_1(out)
         # out = self.bn2_1(out)
@@ -101,8 +101,8 @@ class Ruda_Model(nn.Module):
         # out = self.dropout(out)
 
         # flatten
-        print(out.shape)
-        out = out.view(-1, 24 * 26 * 26)
+        # print(out.shape)
+        out = out.view(-1, 96 * 6 * 6)
 
 
 
